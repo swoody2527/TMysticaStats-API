299e07f1a4981f34799aeeaddff3343233cdf762
Sam Woody <swoodall2527@gmail.com>
2025-07-20 17:15:07 +0100

    CORS

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 
 from flask import Flask
+from flask_cors import CORS
 from page_computations.routes.faction_routes import factions_bp
 from page_computations.routes.maps_routes import maps_bp
 from page_computations.routes.tile_routes import tiles_bp
 from page_computations.routes.trends_routes import trends_bp
 
 app = Flask(__name__)
+CORS(app)
 
 app.register_blueprint(factions_bp, url_prefix='/api/factions')
 app.register_blueprint(maps_bp, url_prefix='/api/maps')
