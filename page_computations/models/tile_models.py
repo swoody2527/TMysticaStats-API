d32ccb98e350551ae6ecabf1657562a7b9421f67
Sam Woody <swoodall2527@gmail.com>
2025-07-25 17:44:05 +0100

    bug fix

--- page_computations/models/tile_models.py
+++ page_computations/models/tile_models.py
@@ -97,8 +97,8 @@ def fetch_vp_gained_by_scoring_tile(s_year, e_year, map=None, num_players=None, 
 
     players_filtered = player_data[(player_data['year'].between(s_year, e_year)) &
                                    (player_data['map'] == map if map else True) &
-                                   (player_data['num_players'] == num_players if num_players else True) &
-                                   (player_data['faction'] == faction if faction else True)].copy()
+                                   (player_data['num_players'] == num_players if num_players is not None else True) &
+                                   (player_data['faction'] == faction if faction is not None else True)].copy()
 
     games_filtered = game_data[['game_id', 'scoring_tiles']].copy()
     df = players_filtered.merge(games_filtered, on='game_id', how='inner')
